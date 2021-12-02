26e9ffd2ac88454ed6aff898781b0652404e7d48
Caleb <tootired4sleep@gmail.com>
2021-12-01 21:36:30 -0500

    adding everything to get us started

---
+++ Assets/Scripts/Gameplay/PlayerEnemyCollision.cs
@@ -0,0 +1,63 @@
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Gameplay
+{
+
+    /// <summary>
+    /// Fired when a Player collides with an Enemy.
+    /// </summary>
+    /// <typeparam name="EnemyCollision"></typeparam>
+    public class PlayerEnemyCollision : Simulation.Event<PlayerEnemyCollision>
+    {
+        public EnemyController enemy;
+        public PlayerController player;
+        public Vector3 scaleChangeEnemy;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            var willHurtEnemy = (player.Bounds.min.y + .05f) >= enemy.Bounds.max.y;
+            Debug.Log("Bounds:");
+            Debug.Log(player.Bounds.center.y);
+            Debug.Log(enemy.Bounds.max.y);
+            
+            if (willHurtEnemy)
+            {
+                var enemyHealth = enemy.GetComponent<Health>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.Decrement();
+                    if (!enemyHealth.IsAlive)
+                    {
+                        Schedule<EnemyDeath>().enemy = enemy;
+                        player.Bounce(2);
+                        scaleChangeEnemy = new Vector2(.2f, .2f);
+                        
+                    }
+                    else
+                    {
+                        player.Bounce(7);
+                    }
+                }
+                else
+                {
+                    Schedule<EnemyDeath>().enemy = enemy;
+                    player.Bounce(2);
+                    scaleChangeEnemy = new Vector2(.2f, .2f);
+                }
+            }
+            else
+            {
+                Schedule<EnemyDeath>().enemy = enemy;
+                scaleChangeEnemy = new Vector2(-.2f, -.2f);
+                Debug.Log("You should get smaller");
+            }
+            player.transform.localScale += scaleChangeEnemy;
+        }
+    }
+}
